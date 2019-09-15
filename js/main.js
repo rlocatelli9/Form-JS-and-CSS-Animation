d29baf0ca0a2e8b99e4496dfcf463e961cb21792
Robson Locatelli <rlocatelli9@gmail.com>
2019-09-15 19:35:15 -0300

    adding new selectors of fields

--- js/main.js
+++ js/main.js
@@ -3,18 +3,37 @@ var btnElement = document.querySelector('button');
 
 btnElement.addEventListener("click", (event) => {
   event.preventDefault();
-  sectionElement.classList.add("form-move");
+
+  var inputElement = [...document.querySelectorAll('.input-block input')];
+  inputElement.forEach(element => {
+    if (element.value === "") {
+      sectionElement.classList.add('form-nono');
+    }
+  });
+
+  var isEmpty = document.querySelector(".form-nono");
+  if (isEmpty) {
+    sectionElement.addEventListener("animationend", (event) => {
+      if (event.animationName == "negative") {
+        isEmpty.classList.remove("form-nono");
+      }
+    });
+  } else {
+    sectionElement.classList.add("form-move");
+  }
+
 });
 
 sectionElement.addEventListener("animationstart", (event) => {
   if (event.animationName === "move_down") {
     document.querySelector("body").style.overflow = "hidden";
   }
-})
+});
 
 sectionElement.addEventListener("animationend", (event) => {
   if (event.animationName === "move_down") {
     sectionElement.style.display = "none";
     document.querySelector("body").style.overflow = "none";
   }
-})
+});
+
