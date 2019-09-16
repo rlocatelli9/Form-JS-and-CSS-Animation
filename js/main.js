2a23307218ba5065af7ee6a0152ab03b1887113a
Robson Locatelli <rlocatelli9@gmail.com>
2019-09-16 13:38:28 -0300

    Create some animated squares.

--- js/main.js
+++ js/main.js
@@ -37,3 +37,26 @@ sectionElement.addEventListener("animationend", (event) => {
   }
 });
 
+/* Animation com squares */
+
+var ulElement = document.querySelector(".squares");
+
+for (let i = 0; i < 11; i++) {
+  var liElement = document.createElement("li");
+  const random = (min, max) => Math.random() * (max - min) + min;
+  const size = Math.floor(random(10, 120));
+  const position = random(1, 99);
+  const delay = random(6, 0.1);
+  const durantion = random(24, 12);
+
+  liElement.style.width = `${size}px`;
+  liElement.style.height = `${size}px`;
+  liElement.style.bottom = `-${size}px`;
+  liElement.style.left = `${position}%`;
+
+  liElement.style.animationDelay = `${delay}s`;
+  liElement.style.animationDuration = `${durantion}s`;
+  liElement.animationTimingFunction = `cubic-bezier(${Math.random()},${Math.random()},${Math.random()},${Math.random()})`
+
+  ulElement.appendChild(liElement);
+}
